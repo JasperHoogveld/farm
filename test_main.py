f071f9576b29bf0bc4777608ecddfe51f6fb1487
Jasper Hoogveld <jasper@hoogveld.org>
2023-04-10 22:36:04 +0200

    Pushing hello world 4

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 from main import index, cow
 
 def test_index(): 
-    assert index() == 'Hello, world 3!'
+    assert index() == 'Hello, world 4!'
     
 def test_cow():
     assert cow() == 'MOoooOo!'
