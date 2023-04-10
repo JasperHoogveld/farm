0c07972d0b7622499d0788066b11c077cef63c92
Jasper Hoogveld <jasper@hoogveld.org>
2023-04-10 22:33:50 +0200

    Pushing hello world 3

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, world 2!'
+    return 'Hello, world 3!'
 
 @app.route('/cow')
 def cow():
