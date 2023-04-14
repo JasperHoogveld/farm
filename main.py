72953a72f35b68454796513759e5ecf9c6e665ad
Jasper Hoogveld <jasper@hoogveld.org>
2023-04-14 14:44:32 +0200

    test yml

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return 'Hello, world 8!' 
+    return 'Hello, world 9!' 
 
 @app.route('/cow')
 def cow():
